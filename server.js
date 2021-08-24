dd3d40ec8ba6ac4acb08aae350ba4cd5e5a9173f
kenneth74-pixel <akenakennethfareal@gmail.com>
2021-08-24 07:01:16 -0700

    init

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const serveStatic = require("serve-static")
-const path = require('path');
+const cors = require("cors");
+const path = require('path')
 const app = express();
 app.use(serveStatic(path.join(__dirname, 'dist')));
+app.use(cors());
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
